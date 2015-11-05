73d00fd32ef4536134f364ddfa7dc95fd3165a57
rajeevrajendran <r.rajendran@nin.knaw.nl>
2015-11-05 12:45:36 +0100

    update

--- Trials/monitor.m
+++ Trials/monitor.m
@@ -1,13 +1,16 @@
-function monitor(diagonal_inch,vertical_resolution,horz_ratio,vert_ratio)
-hyp_cm=diagonal_inch*2.54;
+function monitor(diag_inch,horz_resolution,vert_resolution,horz_ratio,vert_ratio)
+hyp_cm=diag_inch*2.54;
 h=horz_ratio;
 v=vert_ratio;
 unit=(((hyp_cm)^2)/(h^2+v^2))^0.5;
 length=h*unit;
 height=v*unit;
-vres=vertical_resolution/height;
-hres=(vertical_resolution*h/v)/length;
-length
-height
-hres
-vres
+vdpi=vert_resolution/height;
+hdpi=horz_resolution/length;
+p=vert_resolution*horz_resolution;
+X = ['Height ',num2str(height),', Width ',num2str(length),', DPI ',num2str(vdpi),' ',num2str(hdpi),', Pixels ',num2str(p)];
+disp(X)
+
+% disp (['Width is ' ]); disp (length);
+% disp (['Height is ' ]); disp (height);
+% disp (['DPI is ' ]); disp (dpi);
