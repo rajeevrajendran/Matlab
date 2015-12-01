204cb7685ae1dda1d969204624309935a9bcc6f7
rajeevrajendran <r.rajendran@nin.knaw.nl>
2015-12-01 07:20:46 +0100

    update

--- Trials/monitor.m
+++ Trials/monitor.m
@@ -3,10 +3,10 @@ diag_cm=diag_inch*2.54;
 h=horz_ratio;
 v=vert_ratio;
 unit_ratio=(((diag_cm)^2)/(h^2+v^2))^0.5;
-length=h*unit_ratio;
-height=v*unit_ratio;
-vdpi=vert_res/height;
-hdpi=horz_res/length;
-p=vert_res*horz_res;
-X = ['Height is ',num2str(height),' cm, Width is ',num2str(length),' cm, DPI is ',num2str(vdpi),' horz and  ',num2str(hdpi),' vert, Pixels ',num2str(p)];
+length=(h*unit_ratio)/59.7727;
+height=(v*unit_ratio)/33.6221;
+vdpi=(vert_res/height)/(32.1217*33.6221);
+hdpi=(horz_res/length)/(32.1217*59.7727);
+p=(vert_res*horz_res)/2073600;
+X = ['Height is ',num2str(height),' Width is ',num2str(length),' DPI is ',num2str(vdpi),' horz and ',num2str(hdpi),' vert, Pixels ',num2str(p)];
 disp(X)
