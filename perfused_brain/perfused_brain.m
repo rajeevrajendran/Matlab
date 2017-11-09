87f36cb4f720644ef0441dd3b7b84b586fe890a3
rajeevrajendran <r.rajendran@nin.knaw.nl>
2017-11-09 16:04:00 +0100

    added perfused script

---
+++ perfused_brain/perfused_brain.m
@@ -0,0 +1,25 @@
+% Created by Rajeev Rajendran
+% Date created 2016-10-12
+
+%% Open ImageJ
+Miji
+IJ = ij.IJ;
+
+%%
+raw =double(MIJ.getCurrentImage());
+x=512;
+y=796;
+t=1499;
+n=5;
+
+mean_image=[];
+for b=0:298
+    for a=1:n
+        image(:,:,a)=raw(:,:,(5*b+a));
+    end
+    image=(image(:,:,1)+image(:,:,2)+image(:,:,3)+image(:,:,4)+image(:,:,5))/5;
+    mean_image(:,:,b+1)=image;
+    
+end
+
+MIJ.createImage(single(mean_image));
